5184debd20f79f1c3e7235951e8649cb6ce58b8c
Xavier Laviron <xavier.laviron@gmx.fr>
2019-01-28 12:31:17 +0100

    doc for 'theia_download'

--- R/functions.R
+++ R/functions.R
@@ -1,13 +1,35 @@
-setGeneric("theia_download", function(object, override) standardGeneric("theia_download"))
+# ------------------------------------------------------------------------------
+# Generic functions
+# ------------------------------------------------------------------------------
+
+#' Download theia object
+#'
+#' Download an object of class Theia*
+#'
+#' @param override Logical: download files that are already present? (default to
+#' FALSE)
+#'
+#' @return Invisibly, a vector of downloaded file path
+#'
+#' @export
+
+setGeneric("theia_download", function(object, override = FALSE) standardGeneric("theia_download"))
+
+
+#' @describeIn theia_download Download a single tile
+#'
+#' @param object
+#'
+#' @export
 
 setMethod("theia_download", "TheiaTile",
-          function(object, override = F) {
+          function(object, override = FALSE) {
             if (!(file.exists(object@file.path)) | override == TRUE) {
               tryCatch(download.file(object@url, destfile = object@file.path),
                        error = function(e) {
                          warning("Could not download file!")
                        })
             } else {
-              message("File", object@file.path, "already exists, skipping.")
+              message("File ", object@file.path, " already exists, skipping.")
             }
           })
