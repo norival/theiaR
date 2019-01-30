0e0c24bc1cbfa5f6c28f647111aea791f8c76090
Xavier Laviron <xavier.laviron@gmx.fr>
2019-01-30 19:38:01 +0100

    WIP: update TheiaQuery class definition

--- R/TheiaQuery.R
+++ R/TheiaQuery.R
@@ -1,18 +1,75 @@
+#' @export
 TheiaQuery <-
   R6Class("TheiaQuery",
+          # private ------------------------------------------------------------
+          private =
+            list(token    = NULL,
+                 url      = NULL,
+                 catalog  = NULL),
+
           # public -------------------------------------------------------------
           public =
-            list(query  = NULL,
-                 login  = NULL,
-                 passwd = NULL,
+            list(query    = NULL,
+                 tiles    = NULL,
+                 login    = NULL,
+                 passwd   = NULL,
 
                  initialize = function(login, passwd, query)
                  {
                    self$login  <- login
                    self$passwd <- passwd
 
-                   # analyze query
+                   # base url for theia
+                   base.url <- "https://theia.cnes.fr/atdistrib/resto2/api/collections"
+
+                   # how to display in query link
+                   q.link <- c("q",
+                               "location",
+                               "platform",
+                               "startDate",
+                               "completionDate",
+                               "lat",
+                               "lon")
+
+                   # how query is given by the user
+                   q.query <- c("town",
+                                "tile",
+                                "platform",
+                                "start.date",
+                                "end.date",
+                                "latitude",
+                                "longitude")
+
+                   # match search and links
+                   i <- which(q.query %in% names(query))
+                   j <- which(names(query) %in% q.query)
+
+                   # build query links
+                   query.link  <- paste(q.link[i], query[j], sep = "=", collapse = "&")
+                   private$url <- paste0(base.url, "/", query$collection, "/",
+                                         "search.json?", query.link)
+                 },
+
+                 submit = function(...)
+                 {
+                   # TODO: gestion des erreurs
+                   # make http request to get catalog
+                   req <- httr::GET(private$url)
+
+                   # parse and save catalog
+                   private$catalog <- httr::content(req, as = "parsed")
+
+                   # extract tiles
+                   cart <-
+                     lapply(private$catalog$features,
+                            function(x) {
+                              data.frame(file.name = paste0(x$properties$title, ".zip"),
+                                         url       = x$properties$services$download$url,
+                                         file.hash = x$properties$services$download$checksum)
+                            })
+                   self$tiles <- do.call(rbind, cart)
+
+                   return(invisible(self))
                  })
           )
-
 
