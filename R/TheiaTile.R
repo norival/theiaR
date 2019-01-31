e87280141547711cf02548c0cc2c1779605a293e
Xavier Laviron <xavier.laviron@gmx.fr>
2019-01-31 13:02:08 +0100

    clean code for TheiaTile class

--- R/TheiaTile.R
+++ R/TheiaTile.R
@@ -46,71 +46,94 @@ TheiaTile <-
                  url        = NA,
                  status     = list(),
 
-                 initialize = function(file.path,
-                                       url,
-                                       file.hash)
+                 initialize = function(file.path, url, file.hash)
                  {
-                   # Fill fiedls of the object
-                   self$file.path <- file.path
-                   self$url       <- url
-                   self$file.hash <- file.hash
-                   self$status    <- list(exists  = FALSE,
-                                          checked = FALSE,
-                                          correct = NA)
-
-                   # check the tile
-                   self$check()
+                   .TheiaTile_initialize(self, file.path, url, file.hash)
                  },
 
                  print = function(...)
                  {
-                   # Special method to print
-                   # TODO: better method to print
-                   cat("An Tile from Theia\n")
-
-                   return(invisible(self))
+                   .TheiaTile_print(self)
                  },
 
                  check = function()
                  {
-                   # check the tile
-                   if (file.exists(self$file.path)) {
-                     # if the file exists, check it
-                     message("Checking downloaded file...")
-
-                     self$status$exists  <- TRUE
-                     self$status$checked <- TRUE
-
-                     # compute the md5 sum and compare it to the hash
-                     if (tools::md5sum(self$file.path) == self$file.hash) {
-                       self$status$correct <- TRUE
-                     } else {
-                       self$status$correct <- FALSE
-                     }
-                   }
-
-                   return(invisible(self))
+                   .TheiaTile_check(self)
                  },
 
                  download = function(override = FALSE)
                  {
-                   # download the file if it is not present and override is set
-                   # to FALSE
-                   if (!(file.exists(self$file.path)) | override == TRUE) {
-                     tryCatch(download.file(self$url, destfile = self$file.path),
-                              error = function(e) {
-                                warning("Could not download file!")
-                              })
-                   } else {
-                     # The file already exists
-                     message("File ", self$file.path, " already exists, skipping.")
-                   }
-
-                   # check the tile
-                   self$check()
-
-                   return(invisible(self))
+                   .TheiaTile_download(self, override)
                  })
           )
 
 
+# Functions definitions --------------------------------------------------------
+
+.TheiaTile_print <- function(self)
+{
+  # Special method to print
+  # TODO: better method to print
+  cat("An Tile from Theia\n")
+
+  return(invisible(self))
+}
+
+
+.TheiaTile_initialize <- function(self, file.path, url, file.hash)
+{
+  # Fill fiedls of the object
+  self$file.path <- file.path
+  self$url       <- url
+  self$file.hash <- file.hash
+  self$status    <- list(exists  = FALSE,
+                         checked = FALSE,
+                         correct = NA)
+
+  # check the tile
+  self$check()
+
+  return(invisible(self))
+}
+
+
+.TheiaTile_check <- function(self)
+{
+  # check the tile
+  if (file.exists(self$file.path)) {
+    # if the file exists, check it
+    message("Checking downloaded file...")
+
+    self$status$exists  <- TRUE
+    self$status$checked <- TRUE
+
+    # compute the md5 sum and compare it to the hash
+    if (tools::md5sum(self$file.path) == self$file.hash) {
+      self$status$correct <- TRUE
+    } else {
+      self$status$correct <- FALSE
+    }
+  }
+
+  return(invisible(self))
+}
+
+
+.TheiaTile_download <- function(self, override = FALSE)
+{
+  # download the file if it is not present and override is set to FALSE
+  if (!(file.exists(self$file.path)) | override == TRUE) {
+    tryCatch(download.file(self$url, destfile = self$file.path),
+             error = function(e) {
+               warning("Could not download file!")
+             })
+  } else {
+    # The file already exists
+    message("File ", self$file.path, " already exists, skipping.")
+  }
+
+  # check the tile
+  self$check()
+
+  return(invisible(self))
+}
