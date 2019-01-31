d200d044d70fd14645b6dfdcdf02173cfe11b4c0
Xavier Laviron <xavier.laviron@gmx.fr>
2019-01-31 10:02:07 +0100

    WIP: fix methods to download and check Collections

--- R/TheiaCollection.R
+++ R/TheiaCollection.R
@@ -43,40 +43,23 @@ TheiaCollection <-
                              })
 
                      self$tiles <- unname(self$tiles)
-                     # build from query
                    }
                  },
 
-                 check = function()
+                 check = function(...)
                  {
-                   if (file.exists(self$file.path)) {
-                     message("Checking downloaded file...")
-
-                     self$status$exists  <- TRUE
-                     self$status$checked <- TRUE
-
-                     if (tools::md5sum(self$file.path) == self$file.hash) {
-                       self$status$correct <- TRUE
-                     } else {
-                       self$status$correct <- FALSE
-                     }
-                   }
+                   lapply(self$tiles, function(x) x$check())
 
                    return(invisible(self))
                  },
 
                  download = function(override = FALSE)
                  {
-                   if (!(file.exists(self$file.path)) | override == TRUE) {
-                     tryCatch(download.file(self$url, destfile = self$file.path),
-                              error = function(e) {
-                                warning("Could not download file!")
-                              })
-                   } else {
-                     message("File ", self$file.path, " already exists, skipping.")
-                   }
-
-                   self$check()
+                   lapply(self$tiles,
+                          function(x, override) {
+                            x$download(override = override)
+                          },
+                          override = override)
 
                    return(invisible(self))
                  })
