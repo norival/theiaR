ad06f4e4bfde541b0c756d10cd54a42de97c8f92
Xavier Laviron <xavier@norival.dev>
2020-11-14 18:58:08 +0100

    fix evaluation in vignette

--- cran-comments.md
+++ cran-comments.md
@@ -1,15 +1,14 @@
 ## Submission after package archival
 
-* The issue when building the vignette should have been fixed
+* The issue regarding internet resources usage have been fixed
 
 
 ## Test environments
 
 * local ArchLinux install, R 4.0.3
-* Ubuntu 16.04 (on travis-ci), R 3.6.2
+* Ubuntu 16.04 (on travis-ci), R 4.0.2
 * Windows Server 2012 R2 x64, R 4.0.3 (AppVeyor)
-* Windows Server 2008 R2 SP1, R-devel (R-hub)
-* Fedora Linux, R-devel (R-hub)
+* Windows Server 2008 R2 SP1, R-unstable (win-builder)
 
 
 ## R CMD check results
---
+++ vignettes/theiaR.R
@@ -0,0 +1,45 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  devtools::install_github('norival/theiaR')
+#  
+#  # or, to install the development version
+#  devtools::install_github('norival/theiaR', 'devel')
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  isntall.packages('theiaR')
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  library(theiaR)
+#  
+#  # create a list containing the query
+#  myquery <- list(collection = "SENTINEL2",
+#                  town       = "Grenoble",
+#                  start.date = "2018-07-01",
+#                  end.date   = "2018-07-06")
+#  
+#  # create a collection from the query
+#  mycollection <- TheiaCollection$new(query = myquery, dir.path = ".", check = TRUE)
+#  
+#  # check available tiles fro the query
+#  mycollection$status
+#  
+#  # download the tiles into 'dir.path'
+#  mycollection$download(auth = "path/to/auth/file.txt")
+
+## ---- eval=T------------------------------------------------------------------
+myquery <- list(collection = "SENTINEL2",
+                town       = "Grenoble",
+                start.date = "2018-07-01",
+                end.date   = "2018-07-06")
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  mycollection <- TheiaCollection$new(query = myquery, dir.path = ".", check = TRUE)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  print(mycollection)
+
